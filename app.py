4d9a1b30fb5d45f8b8f74dfd1ea36b8696452394
T.H.Sreekar Preetham <preetham@THSreekars-MacBook-Air.local>
2026-05-13 23:02:09 +0530

    Fixed mail password

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'thsreekarp@gmail.com'
-app.config['MAIL_PASSWORD'] = 'task bqfn bbqf tugw'
+app.config['MAIL_PASSWORD'] = 'taskbqfnbbqftugw'
 
 mail = Mail(app)
 
