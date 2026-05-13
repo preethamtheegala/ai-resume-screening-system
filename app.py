7e07bf0d2de33552513cc57c2782a79d0fb5653e
T.H.Sreekar Preetham <preetham@THSreekars-MacBook-Air.local>
2026-05-13 23:09:54 +0530

    Fixed SMTP SSL configuration

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'thsreekarp@gmail.com'
 app.config['MAIL_PASSWORD'] = 'taskbqfnbbqftugw'
 
